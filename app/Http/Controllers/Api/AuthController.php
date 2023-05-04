4673d43de4c4ffc026b5bc9000aba033f9297c3e
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-05-04 23:08:35 +0200

    Modified feartures

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -15,7 +15,6 @@ class AuthController extends Controller
     public function signup(SignupRequest $request)
     {
         $data = $request->validated();
-        /** @var \App\Models\User $user */
         $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
@@ -35,7 +34,6 @@ class AuthController extends Controller
             ], 422);
         }
 
-        /** @var \App\Models\User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
         return response(compact('user', 'token'));
@@ -43,7 +41,7 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-        /** @var \App\Models\User $user */
+
         $user = $request->user();
         $user->currentAccessToken()->delete();
         return response('', 204);
--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -1,9 +1,17 @@
 import axios from "axios";
-import {useStateContext} from "./context/ContextProvider.jsx";
+
 
 const axiosClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
+
+
+
+
+
+
+
+
 
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('ACCESS_TOKEN');
