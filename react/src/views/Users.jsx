a8c1817114c9c0cca6b96ec3666aa244c94fe578
Ezzahir Taha <taha1.ezzahir@gmail.com>
2023-06-02 23:52:36 +0200

    Added pagination functionality using the react-paginate package

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,85 +1,114 @@
-import {useEffect, useState} from "react";
-import axiosClient from "../axios-client.js";
-import {Link} from "react-router-dom";
-import {useStateContext} from "../context/ContextProvider.jsx";
+import React, { useEffect, useState } from 'react';
+import ReactPaginate from 'react-paginate';
+import axiosClient from '../axios-client.js';
+import { Link } from 'react-router-dom';
+import { useStateContext } from '../context/ContextProvider.jsx';
 
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
-  const {setNotification} = useStateContext()
+  const [currentPage, setCurrentPage] = useState(0); // New state for current page
+  const perPage = 5; // Number of items per page
+  const { setNotification } = useStateContext();
 
   useEffect(() => {
     getUsers();
-  }, [])
+  }, [currentPage]); // Update the user list when the current page changes
 
-  const onDeleteClick = user => {
-    if (!window.confirm("Are you sure you want to delete this user?")) {
-      return
+  const onDeleteClick = (user) => {
+    if (!window.confirm('Are you sure you want to delete this user?')) {
+      return;
     }
-    axiosClient.delete(`/users/${user.id}`)
+    axiosClient
+      .delete(`/users/${user.id}`)
       .then(() => {
-        setNotification('User was successfully deleted')
-        getUsers()
-      })
-  }
+        setNotification('User was successfully deleted');
+        getUsers();
+      });
+  };
 
   const getUsers = () => {
-    setLoading(true)
-    axiosClient.get('/users')
+    setLoading(true);
+    const page = currentPage + 1; // API pages start from 1, not 0
+    axiosClient
+      .get(`/users?page=${page}&per_page=${perPage}`)
       .then(({ data }) => {
-        setLoading(false)
-        setUsers(data.data)
+        setLoading(false);
+        setUsers(data.data);
+        console.log(data);
       })
       .catch(() => {
-        setLoading(false)
-      })
-  }
+        setLoading(false);
+      });
+  };
+
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected);
+  };
 
   return (
     <div>
-      <div style={{display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <h1>Users</h1>
-        <Link className="btn-add" to="/users/new">Add new</Link>
+        <Link className="btn-add" to="/users/new">
+          Add new
+        </Link>
       </div>
       <div className="card animated fadeInDown">
         <table>
           <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Create Date</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          {loading &&
-            <tbody>
             <tr>
-              <td colSpan="5" className="text-center">
-                Loading...
-              </td>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
             </tr>
-            </tbody>
-          }
-          {!loading &&
+          </thead>
+          {loading && (
             <tbody>
-            {users.map(u => (
-              <tr key={u.id}>
-                <td>{u.id}</td>
-                <td>{u.name}</td>
-                <td>{u.email}</td>
-                <td>{u.created_at}</td>
-                <td>
-                  <Link className="btn-edit" to={'/users/' + u.id}>Edit</Link>
-                  &nbsp;
-                  <button className="btn-delete" onClick={ev => onDeleteClick(u)}>Delete</button>
+              <tr>
+                <td colSpan="5" className="text-center">
+                  Loading...
                 </td>
               </tr>
-            ))}
             </tbody>
-          }
+          )}
+          {!loading && (
+            <tbody>
+              {users.map((u) => (
+                <tr key={u.id}>
+                  <td>{u.id}</td>
+                  <td>{u.name}</td>
+                  <td>{u.email}</td>
+                  <td>{u.created_at}</td>
+                  <td>
+                    <Link className="btn-edit" to={'/users/' + u.id}>
+                      Edit
+                    </Link>
+                    &nbsp;
+                    <button className="btn-delete" onClick={(ev) => onDeleteClick(u)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={totalUsers ? Math.ceil(totalUsers / perPage) : 0}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
     </div>
-  )
+  );
 }
